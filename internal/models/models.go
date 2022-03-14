3f46b90ea41ad801963d4c719014ca4e373303b1
Rob Thorne <rob@torenware.com>
2022-03-14 13:14:05 -0700

    formatted a type

--- internal/models/models.go
+++ internal/models/models.go
@@ -44,18 +44,18 @@ type Widget struct {
 
 // Order is the type for all orders
 type Order struct {
-	ID            int       `json:"id"`
-	WidgetID      int       `json:"widget_id"`
-	TransactionID int       `json:"transaction_id"`
-	CustomerID    int       `json:"customer_id"`
-	StatusID      int       `json:"status_id"`
-	Quantity      int       `json:"quantity"`
-	Amount        int       `json:"amount"`
-	CreatedAt     time.Time `json:"created_at"`
-	UpdatedAt     time.Time `json:"-"`
-	Widget	Widget `json:"widget"`
-	Transaction Transaction `json:"transaction"`
-	Customer Customer  `json:"customer"`
+	ID            int         `json:"id"`
+	WidgetID      int         `json:"widget_id"`
+	TransactionID int         `json:"transaction_id"`
+	CustomerID    int         `json:"customer_id"`
+	StatusID      int         `json:"status_id"`
+	Quantity      int         `json:"quantity"`
+	Amount        int         `json:"amount"`
+	CreatedAt     time.Time   `json:"created_at"`
+	UpdatedAt     time.Time   `json:"-"`
+	Widget        Widget      `json:"widget"`
+	Transaction   Transaction `json:"transaction"`
+	Customer      Customer    `json:"customer"`
 }
 
 // Status is the type for order statuses
